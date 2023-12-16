7f3f3ad8ac153bd8883f0ce39edbd380e8885af7
ritikrishuRP <66078412+ritikrishuRP@users.noreply.github.com>
2023-12-16 18:03:43 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+// Always compare with same datatype
+console.log(null>0)    //fasle
+console.log(null==0)   //false
+consolelog(null>=0)  //true
+
+//undefined gives false in all case
+//always gives unpredictable result
+
+//=== it also check data-type
