b1b41ace551c6fc2015ccc27d9a06a988ca9dbd7
ritikrishuRP <66078412+ritikrishuRP@users.noreply.github.com>
2023-12-16 17:55:42 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,3 +22,38 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+// ******************** Operations *******************
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "Ritik"
+let str2 = " Ritik"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)       //12
+console.log(1 + "2")       //12
+console.log("1" + 2 + 2)   //122
+console.log(1 + 2 + "2")   //32
+
+console.log(+true) //1
+console.log(+"")   //0
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+
+
+
+
+
+
+
+
+
+
+
+
