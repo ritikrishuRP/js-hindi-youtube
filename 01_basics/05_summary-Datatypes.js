1b6194ed84ce5195b12808734898ef90cdda4fb9
ritikrishuRP <66078412+ritikrishuRP@users.noreply.github.com>
2023-12-16 18:21:03 +0000

    summary

---
+++ 01_basics/05_summary-Datatypes.js
@@ -0,0 +1,41 @@
+// # primitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol (helps to make value unique), BigInt
+
+// # reference 
+
+//Array, object, function
+
+// JavaScript is a Dynamically-typed languages are those (like JavaScript) where the interpreter
+// assigns variables a type at runtime based on the variable's value at the time.
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId)
+
+const bigNumber = 47635493423483n
+
+const Heroes = ["shaktiman", "nagraz", "doga"]
+
+let myObj = {
+    name: "hitesh",
+    age: 22
+}
+
+const myFunc = function(){
+    console.log("Hello world");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
