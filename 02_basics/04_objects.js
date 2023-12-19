1d92e44a8efa96940f95c3ba5c8c8c42779ef00e
ritikrishuRP <66078412+ritikrishuRP@users.noreply.github.com>
2023-12-19 18:02:23 +0000

    API

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,13 +1,13 @@
 //const tinderUser = new Object() //{}
 const tinderUser = {}
 
-console.log(tinderUser)
+console.log(tinderUser) //{}
 
 tinderUser.id = "123abc"
 tinderUser.name = "ritik"
 tinderUser.isLoggedIn = false
 
-console.log(tinderUser)
+console.log(tinderUser) //{ id: '123abc', name: 'ritik', isLoggedIn: false }
 
 const regularUser = {
     email: "some@gmail.com",
@@ -20,4 +20,52 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullName)
+console.log(regularUser.fullName) //{ userfullname: { firstName: 'hitesh', lastName: 'chowdhary' } }
+console.log(regularUser.fullName.userfullname.firstName)
+
+const obj1 = {1: "a", 2: "c"}
+const obj2 = {3: "a", 4: "d"}
+
+//const obj3 = {obj1, obj2} //{ obj1: { '1': 'a', '2': 'c' }, obj2: { '3': 'a', '4': 'd' } }
+//const obj3 = Object.assign({}, obj1, obj2) 
+//console.log(obj1) //{ '1': 'a', '2': 'c' }
+//console.log(obj3) //{ '1': 'a', '2': 'c', '3': 'a', '4': 'd' }
+
+const obj3 = {...obj1, ...obj2} //mostly used
+
+//database generally send array of object
+
+const user =[
+    {
+        id : "asasa@gmail.com",
+        name: "pta nhi"
+    },
+    {
+        id : "asasa@gmail.com",
+        name: "yhi hain"
+    },
+    {
+        id : "asasa@gmail.com",
+        name: "pta nhi"
+    }
+] 
+console.log(user[1].name)
+
+console.log(tinderUser)   //{ id: '123abc', name: 'ritik', isLoggedIn: false }
+
+console.log(Object.keys(tinderUser)) //[ 'id', 'name', 'isLoggedIn' ]
+console.log(Object.values(tinderUser))  //[ '123abc', 'ritik', false ]
+console.log(Object.entries(tinderUser)) //[ [ 'id', '123abc' ], [ 'name', 'ritik' ], [ 'isLoggedIn', false ] ]
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')) //boolean //true
+
+const course = {
+    name : "ritik",
+    courseName: "chai aur code",
+    instructor: "hitesh"
+}
+
+const {instructor : teacher} = course
+
+console.log(instructor) //hitesh
+console.log(teacher) //hitesh
